d73cc48d22243a72608bba769a4d4cf5a8192200
JohanDC-1999 <johan.00444@gmail.com>
2024-01-20 18:58:13 +0200

    Added ability to track score. Also added function to show the answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// Declare a global variable for the score and initialize it to zero
+var score = 0;
+
+// Declare global variable to access the answer on-demand instead of re-doing the calculation
+var answer;
+
 // Returns a random integer between min and max (inclusive)
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,12 +16,14 @@ function createMultiplicationEquation() {
     let num2 = getRandomInt(1, 12);
     // Calculate the product
     let product = num1 * num2;
+    // Update the answer
+    answer = product;
     // Return the equation and the product as an object
-    return { equation: `${num1} x ${num2}`, product: product };
+    return { equation: `${num1} x ${num2} = `, product: product };
 }
 
 // Function to toggle the disabled attribute of the 'submit' button
-function toggleDisabledSubmit(isDisabled){
+function toggleDisabledSubmit(isDisabled) {
     document.getElementById("submit").disabled = isDisabled;
 }
 
@@ -31,8 +39,29 @@ function nextQuestion() {
     document.getElementById("feedback").innerHTML = "";
 }
 
+// Function to show the answer
+function showAnswer() {
+    let equation = document.getElementById("equation").innerHTML;
+    var regex = /(.*) = /; // match any characters before the = sign and capture them in a group
+    var match = equation.match(regex); // apply the regex to the equation
+    if (match) { // if there is a match
+        var result = match[1]; // get the first captured group
+        // use eval to print the result 
+        console.log(result); // print the result
+        document.getElementById("input").value = eval(result.replace('x', '*')); // replace * with x to work with eval
+    }
+}
+
+// Function to show the answer
+function showAnswerSimple() {
+    document.getElementById("input").value = answer;
+}
+
 // Check if the answer entered is correct
 function checkAnswer() {
+    // Only check if button is not yet disabled - if they don't have the answer correct yet
+    if (document.getElementById("submit").disabled === true)
+        return
     // Get the user's input
     let input = document.getElementById("input").value;
     // Convert it to a number
@@ -42,6 +71,8 @@ function checkAnswer() {
         // If correct, show a positive feedback
         document.getElementById("feedback").innerHTML = "Correct!";
         toggleDisabledSubmit(true);
+        score++;
+        console.log(score);
     } else {
         // If incorrect, show a negative feedback
         document.getElementById("feedback").innerHTML = "Wrong!";
